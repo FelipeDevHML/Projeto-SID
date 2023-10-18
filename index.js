a0cc321738ff551e17da26a9e7d4e43ececc79d8
Andre Felipe Araujo <FelipeDevHML>
2023-10-18 00:10:38 -0300

    Pegadinha até chegar o PIX para apaziguar a vontade de tirar o site do AR

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ botoesLeiaMais.forEach((botao, index) => {
         }
     });
 });
+
+
+// const pega = document.querySelector('#bodyPegadinha');
+
+// pegadinha = () => {
+//     pega.innerHTML = '<img src="./assets/img/imeretian-khachapuri-cheese-lemon-side-view.jpg" alt="PEGADINHA" style="width: 100hv;">'
+// }
+
+
+
+const dialog = document.querySelector("dialog");
+const closeButton = document.querySelector("dialog button");
+
+// "Show the dialog" button opens the dialog modally
+setInterval(() => {
+    dialog.showModal();
+}, 15*1000);
+
+// "Close" button closes the dialog
+closeButton.addEventListener("click", () => {
+  dialog.close();
+});
